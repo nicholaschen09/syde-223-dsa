0f78e0b3423accc47a48064d17613d5a7d20cd03
nicholaschen09 <nicholas.chen243@gmail.com>
2025-05-08 11:05:45 -0400

    init commit

---
+++ .history/test_20250508110455.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    
+    // Test some basic arithmetic
+    int a = 5;
+    int b = 3;
+    std::cout << "Sum of " << a << " and " << b << " is: " << a + b << std::endl;
+    std::cout << "Difference of " << a << " and " << b << " is: " << a - b << std::endl;
+    std::cout << "Product of " << a << " and " << b << " is: " << a * b << std::endl;
+    
+    return 0;
+}
